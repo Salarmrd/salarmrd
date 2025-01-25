aa0ea45f91d6e27143990a576138f3b1e5004e1c
Amirsalar <Amirsalarmehrdad22@gmail.com>
2025-01-25 14:19:32 +0300

    Navbar test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import "./Navbar.scss";
 import { HashLink } from "react-router-hash-link";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  // useState must be inside the functional component
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar navbar-expand-lg py-3">
       <div className="container">
@@ -13,15 +16,11 @@ const Navbar = () => {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={() => setIsOpen(!isOpen)} // Toggle the state
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <HashLink className="nav-link active" to="#about">
@@ -33,13 +32,8 @@ const Navbar = () => {
                 Skills
               </HashLink>
             </li>
-            {/* <li className="nav-item">
-              <Link className="nav-link" to="/info">
-                Info
-              </Link>
-            </li> */}
             <li className="nav-item">
-            <HashLink className="btn" to="#contact">
+              <HashLink className="btn" to="#contact">
                 Contact me
               </HashLink>
             </li>
