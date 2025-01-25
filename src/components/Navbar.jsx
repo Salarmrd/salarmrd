dcc540edc2f811e8fef28eb6c9226162227e0a77
Amirsalar <Amirsalarmehrdad22@gmail.com>
2025-01-25 14:27:44 +0300

    fixed the navbar animation for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  // useState must be inside the functional component
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -14,12 +13,19 @@ const Navbar = () => {
           Salarmrd.
         </Link>
         <button
-          className="navbar-toggler"
-          type="button"
-          onClick={() => setIsOpen(!isOpen)} // Toggle the state
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+  className="navbar-toggler"
+  type="button"
+  onClick={() => setIsOpen(!isOpen)} // Toggle isOpen state
+  aria-expanded={isOpen}
+  aria-label="Toggle navigation"
+>
+  {/* Conditional rendering for the menu icon */}
+  {isOpen ? (
+    <span className="close-icon">✖</span> // Cross icon
+  ) : (
+    <span className="navbar-toggler-icon"></span> // Default burger icon
+  )}
+</button>
         <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
